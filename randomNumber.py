e529db5b6135408916f4b085c3b051d4bc3553e6
Andrew Allison <andrewallison5211@gmail.com>
2024-06-18 15:03:53 -0600

    random number

--- randomNumber.py
+++ randomNumber.py
@@ -1,2 +1,15 @@
 import random
 
+def guess(x):
+  random_number = random.randint(1, x)
+  guess = 0
+  while guess != random_number:
+    guess = int(input(f"Guess a number between 1 and {x}: "))
+    if guess < random_number:
+      print("Guess to low. Try again.")
+
+  
+  if guess == random_number:
+    print('You got it!')
+  else:
+    print('Sorry, you are wrong')
