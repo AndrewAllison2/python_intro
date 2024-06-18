2f31148210b17cc1b454ff28dfdffd78812cba20
Andrew Allison <andrewallison5211@gmail.com>
2024-06-18 14:57:13 -0600

    string cat

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+youtuber = 'Altrnt'
+
+print(f"Subscribe to {youtuber}")
+
+adj = input('Adjective: ')
+verb1 = input('Verb: ')
+verb2 = input('Verb: ')
+famous_person = input('Famous Person: ')
+
+madlib = f"I'm having a {adj} day. I need to {verb1} my laundry and {verb2} my dogs still. {famous_person} is coming over for dinner later too."
+
+print(madlib)
